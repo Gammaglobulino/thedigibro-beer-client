b45bbd9034ae44ebc6ebafd69916beef231a1adc
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2019-09-09 15:23:16 +0200

    Initial commit

---
+++ src/main/java/com/thedigibro/thedigibrobeerclient/ThedigibroBeerClientApplication.java
@@ -0,0 +1,13 @@
+package com.thedigibro.thedigibrobeerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThedigibroBeerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThedigibroBeerClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thedigibro/thedigibrobeerclient/ThedigibroBeerClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thedigibro.thedigibrobeerclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThedigibroBeerClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
